5d7b7f227a079a1b13a8ffc82b74de21fa6628a5
Hendardinata <muhammadhifzil19@gmail.com>
2025-10-15 22:42:24 +0800

    Update server

--- server.js
+++ server.js
@@ -19,4 +19,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server Berjalan di PORT ${PORT}'));
+app.listen(PORT, () => console.log(`Server Berjalan di PORT ${PORT}`));
